12f7a06e1d7f5204a6cde0483f6dc421b27bfb71
Hyeonss <hyeonss0417@gmail.com>
2023-11-30 15:19:04 +0900

    fix(app): 모듈 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,32 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 
-import { AppController } from './app.controller';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
+import { FeedsModule } from './feeds/feeds.module';
 import { PagesModule } from './pages/pages.module';
 import { PostsModule } from './posts/posts.module';
 import { PrismaService } from './prisma.service';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { ValidatorExceptionFilter } from './validator.exception';
 
 @Module({
-  imports: [AuthModule, PagesModule, PostsModule],
-  controllers: [AppController],
+  imports: [
+    AuthModule,
+    PagesModule,
+    PostsModule,
+    SubscriptionsModule,
+    FeedsModule,
+  ],
   providers: [
     PrismaService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ValidatorExceptionFilter,
     },
   ],
 })
